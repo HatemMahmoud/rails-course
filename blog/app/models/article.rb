fc2b9db141a5f6ec9e83af217bcbf86ff48d5f32
Hatem Mahmoud <hatem@expressionlab.com>
2012-09-14 11:55:23 +0200

    Updated scaffolding and added validation

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ActiveRecord::Base
   attr_accessible :content, :title
+  
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+  
+  validates :content, :presence => true
 end
